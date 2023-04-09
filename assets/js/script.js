3e8c6155c7a5fe3890e14bced4f1e989f765b179
Murph25 <rebm_90@yahoo.ie>
2023-04-09 15:30:16 +0000

    add func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log("connected!");
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+    
+}
+function displayMultiplyQuestion(){
+    
+}
+function displayDivisionQuestion(){
+    
+}
