9781112c4ba70488cff2b3a0bb3e77dee54c6a57
Murph25 <rebm_90@yahoo.ie>
2023-04-09 15:53:59 +0000

    add run game structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,47 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
+function checkAnswer() {
 
 }
-function checkAnswer(){
 
-}
-function calculateCorrectAnswer(){
-
-}
-function incrementScore(){
+function calculateCorrectAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function incrementScore() {
 
 }
-function displayAdditionQuestion(){
+
+function incrementWrongAnswer() {
 
 }
-function displaySubtractQuestion(){
-    
+
+function displayAdditionQuestion() {
+
 }
-function displayMultiplyQuestion(){
-    
+
+function displaySubtractQuestion() {
+
 }
-function displayDivisionQuestion(){
+
+function displayMultiplyQuestion() {
     
 }
